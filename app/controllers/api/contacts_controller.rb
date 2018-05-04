a65cc18b085d47f26785d708aecc464ce90c86e5
mpotts0201 <mpotts0201@gmail.com>
2018-05-04 18:42:52 -0400

    created contacts controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,50 @@
 class Api::ContactsController < ApplicationController
+
+    before_action :authenticate_user!
+
+  def index
+    @contacts = current_user.contacts
+
+    render json: @contacts
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+
+    render json: @contact
+  end
+
+  def create
+    @user = current_user
+    @contact = @user.contacts.build(contact_params)
+
+    if @user.save
+      render json: @contact, status: :created, location: @contact
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id]).delete
+
+    render status: :ok
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :contactType, :email, :phone, :address)
+  end
 end
